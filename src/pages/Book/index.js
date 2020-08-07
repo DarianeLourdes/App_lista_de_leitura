56ae139a377f08d6cafaf701ebb86e6110a39b33
darianelourdes <darianelsantana@gmail.com>
2020-08-07 18:36:43 -0300

    feat: working with forms

--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from './styles';
 
 export default function Book({navigation}) {
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [photo, setPhoto] = useState();
+
   return (
     <View style={styles.container}>
       <Text style={styles.pageTitle}>Inclua seu novo livro...</Text>
       <TextInput 
         style={styles.input}
         placeholder="Titulo"
+        value={title}
+        onChangeText={(text) => {
+          setTitle(text)
+        }}
       />
       <TextInput
         style={styles.input}
         placeholder="Descrição"
         multiline={true}
         numberOfLines={4}
+        value={description}
+        onChangeText={(text) => {
+          setDescription(text)
+        }}
       />
 
       <TouchableOpacity style={styles.cameraButton}>
